dc4b2f6715f250bf83bb25f4f8ab4b28ec423e74
Jiook Cha <cha.jiook@gmail.com>
2017-06-15 16:59:01 -0400

    Create mr_1_preproc.m

--- mr_1_preproc.m
+++ mr_1_preproc.m
@@ -1,4 +1,13 @@
 function monk_mrtrix_1_preproc(order)
+
+%------------ FreeSurfer -----------------------------%
+fshome = getenv('FREESURFER_HOME');
+fsmatlab = sprintf('%s/matlab',fshome);
+if (exist(fsmatlab) == 7)
+    path(path,fsmatlab);
+end
+    
+
 %% reset LD_LIBRARY_PATH
 LD_LIBRARY_PATH=['/nfs/apps/gcc/4.9.2/lib64'];
 %/home/juke/MATLAB/R2016b/sys/os/glnxa64:
@@ -6,7 +15,7 @@ LD_LIBRARY_PATH=['/nfs/apps/gcc/4.9.2/lib64'];
 setenv('LD_LIBRARY_PATH', LD_LIBRARY_PATH)
 !echo $LD_LIBRARY_PATH
 
-parentdir='/ifs/scratch/pimri/posnerlab/1anal/Monk/data';
+parentdir='/ifs/scratch/pimri/posnerlab/1anal/highrisk/fs';
 
 [num,txt,raw] = xlsread(fullfile(parentdir,'list_good_dwi.xls'));
 
@@ -29,7 +38,7 @@ cd(workingdir)
 
 %% 1. setup %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-cd('DTI')
+cd('dmri2')
 dwiname=txt(order+1,3)
 cmd=['find . -maxdepth 1 -name "' dwiname{1} '"'] 
 [status, out]=system(cmd)
@@ -40,47 +49,48 @@ img_dwi=out(1:length(out)-1)
 
 
 C=regexp(dwiname{1},'[.]','split')
-img_bvecs=fullfile(pwd,[C{1} '.bvec'])
-img_bvals=fullfile(pwd,[C{1} '.bval'])
+img_bvecs=fullfile(pwd,[C{1} '.bvecs'])
+img_bvals=fullfile(pwd,[C{1} '.bvals'])
 
 
-  if ~exist('mr_fod.mif')
+  if ~exist('mr_fod.mif.gz')
 %% 2. DWI processing2-converting nifti to mif%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-cmd = (['mrconvert ' img_dwi ' -force mr_dwi.mif -fslgrad ' img_bvecs ' ' img_bvals ' -datatype float32 -stride 0,0,0,1'])
+cmd = (['mrconvert ' img_dwi ' -force mr_dwi.mif.gz -fslgrad ' img_bvecs ' ' img_bvals ' -datatype float32 -stride 0,0,0,1'])
 unix(cmd)
 
 %% denoising
-cmd = (['dwidenoise mr_dwi.mif -force mr_dwi_denoised.mif -nthreads 4']);
+cmd = (['dwidenoise mr_dwi.mif.gz -force mr_dwi_denoised.mif.gz']);
 unix(cmd)
 
 %% dwipreproc -eddy current
-cmd = (['dwipreproc PA mr_dwi_denoised.mif mr_dwi_denoised_preproc.mif -rpe_none -force'])
+cmd = (['dwipreproc PA mr_dwi_denoised.mif.gz mr_dwi_denoised_preproc.mif.gz -rpe_none -force'])
 unix(cmd)
 
 %% mask and bias field correction
 % img_dwi_biasCorr=fullfile(workingdir,'dwi_denoised_biasCorr.nii.gz');
-unix(['dwi2mask mr_dwi_denoised_preproc.mif - | maskfilter - erode -npass 7 -force mr_eroded_mask.mif'])
+unix(['dwi2mask mr_dwi_denoised_preproc.mif.gz - | maskfilter - erode -npass 7 -force mr_eroded_mask.mif.gz'])
 
-cmd = (['dwibiascorrect mr_dwi_denoised_preproc.mif -force mr_dwi_denoised_preproc_biasCorr.mif -ants -mask mr_eroded_mask.mif -fslgrad ' img_bvecs ' ' img_bvals ' -nthreads 4']);
+cmd = (['dwibiascorrect mr_dwi_denoised_preproc.mif.gz -force mr_dwi_denoised_preproc_biasCorr.mif.gz -ants -mask mr_eroded_mask.mif.gz -fslgrad ' img_bvecs ' ' img_bvals ' -nthreads 4']);
 unix(cmd) 
+
 
 
 %% DWI processing3- bad vol exclusion %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%% if stripe noise is found in a volume, make sure you delete %%%%
-range=txt(order+1,6)
+%range=txt(order+1,6)
 
-cmd=['mrconvert -coord 3 ' range{1} ' mr_dwi_denoised_preproc_biasCorr.mif -force mr_dwi_denoised_preproc_biasCorr_reduced.mif']
-unix(cmd)
+%cmd=['mrconvert -coord 3 ' range{1} ' mr_dwi_denoised_preproc_biasCorr.mif -force mr_dwi_denoised_preproc_biasCorr_reduced.mif']
+%unix(cmd)
 
 
 %%%% generating b0 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-unix('dwiextract mr_dwi_denoised_preproc_biasCorr_reduced.mif - -bzero | mrmath - mean -force mr_meanb0.mif -axis 3')
+unix('dwiextract mr_dwi_denoised_preproc_biasCorr.mif.gz - -bzero | mrmath - mean -force mr_meanb0.mif.gz -axis 3')
 
 %% upsampling %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-filelist={'mr_dwi_denoised_preproc_biasCorr_reduced','mr_eroded_mask','mr_meanb0'};
+filelist={'mr_dwi_denoised_preproc_biasCorr','mr_eroded_mask','mr_meanb0'};
 for i=1:length(filelist)
-    im=[filelist{i} '.mif'];
-    newim=[filelist{i} '_upsample.mif'];
+    im=[filelist{i} '.mif.gz'];
+    newim=[filelist{i} '_upsample.mif.gz'];
     cmd=['mrresize ' im ' -scale 2.0 -force ' newim];
     unix(cmd)
 end
@@ -91,24 +101,33 @@ end
 
 %%%% eroded mask
 s
-cmd=['dwi2mask mr_dwi_denoised_reduced.mif - | maskfilter - erode -npass 7 mr_eroded_mask.mif -force']
+cmd=['dwi2mask mr_dwi_denoised.mif.gz - | maskfilter - erode -npass 7 mr_eroded_mask.mif.gz -force']
 unix(cmd)
 
+
 %%%% response
-cmd=['dwi2response tournier -mask mr_eroded_mask.mif -voxels mr_voxels_reduced_eroded.mif mr_dwi_denoised_preproc_biasCorr_reduced.mif mr_dwi_response_tournier_reduced_eroded.txt -force'];
+cmd=['dwi2response dhollander -mask mr_eroded_mask.mif.gz -voxels mr_voxels_eroded.mif.gz mr_dwi_denoised_preproc_biasCorr.mif.gz response_wm.txt response_gm.txt response_csf.txt -force'];
 unix(cmd)
 
 %% global intensity normalization  THIS MIGHT BE SKIPPED
 
 
+%% 5TTGEN
+unix('mri_convert ./mri/T1.mgz tmp.nii.gz')  
+unix('flip4fsl tmp.nii.gz T1_flip.nii.gz')
+unix('5ttgen fsl T1_flip.nii.gz 5tt.mif.gz')
+
+
 %% FOD%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % make sure to use "DILATED MASK" for FOD generation
-unix(['dwi2mask mr_dwi_denoised_preproc_biasCorr_reduced.mif - | maskfilter - dilate -npass 5 -force mr_dilate_mask.mif'])
+unix(['dwi2mask mr_dwi_denoised_preproc_biasCorr.mif.gz - | maskfilter - dilate -npass 5 -force mr_dilate_mask.mif.gz'])
 % unix(['mrresize mr_dilate_mask.mif -scale 2.0 mr_dilate_mask_upsample.mif'])
 
-cmd1=['dwi2fod csd mr_dwi_denoised_preproc_biasCorr_reduced.mif mr_dwi_response_tournier_reduced_eroded.txt mr_fod.mif -mask mr_dilate_mask.mif -nthreads 4 -force'];
+cmd1=['dwi2fod msmt_csd mr_dwi_denoised_preproc_biasCorr.mif.gz response_wm.txt WM_FODs.mif.gz response_gm.txt gm.mif.gz response_csf.txt csf.mif.gz -mask mr_dilate_mask.mif.gz -force'];
 unix(cmd1)
 
+cmd2=['mrconvert WM_FODs.mif.gz - -coord 3 0 | mrcat csf.mif.gz gm.mif.gz - tissueRGB.mif.gz -axis 3'];
+unix(cmd2)
 disp('I THINK EVERYTHING IS DONE BY NOW')
 end
 
